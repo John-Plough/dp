de664793684177223fc4331fa17a19d30c85f4fd
John-Plough <jplough@gmail.com>
2025-03-15 15:39:04 -0400

    03-loops2.md

--- dp.js
+++ dp.js
@@ -1,16 +1,47 @@
-let city1 = "Rochester";
-let city2 = "Fargo";
-let city3 = "Worcester";
+// Start with an array of numbers and create a new array with each number times 3.
+// For example, [1, 2, 3] becomes [3, 6, 9].
 
-console.log(`Ok, I'll visit ${city1}, ${city2}, and ${city3}.`);
+const nums = [1,2,3];
+console.log(nums.map(num => num * 3));
 
-let pw = window.prompt("Enter pw: ");
-if (pw === "Joshua") {
-  console.log("Shall...?");
-} else {
-  console.log("Denied.");
-}
+// Start with an array of strings and create a new array with each string upcased.
+// For example, ["hello", "goodbye"] becomes ["HELLO", "GOODBYE"].
 
-let title = "Catcher in the Rye";
-let author = "JD Salinger";
-console.log(`${title} was def written by ${author}.`);
+const strs = ["hello", "goodbye"];
+const upcased = strs.map(str => str.toUpperCase());
+console.log(upcased);
+
+// Start with an array of hashes and create a new array of string values from each hash's :name key.
+// For example, [{name: "Alice", age: 27}, {name: "Blane", age: 16}] becomes ["Alice", "Blane"].
+
+const people = [{name: "Alice", age: 27}, {name: "Blane", age: 16}];
+const names = people.map(person => person.name);
+console.log(names);
+
+// Start with an array of numbers and create a new array with each number plus 7.
+// For example, [1, 2, 3] becomes [8, 9, 10].
+
+const m = [1,2,3];
+const mo = m.map(num => num + 7);
+console.log(mo);
+
+// Start with an array of strings and create a new array with each string's length.
+// For example, ["hello", "goodbye"] becomes [5, 7].
+
+const words = ["hello", "goodbye"];
+console.log(words.map(word => word.length));
+
+// Start with an array of hashes and create a new array of number values from each hash's :age key.
+// For example, [{name: "Alice", age: 27}, {name: "Blane", age: 16}] becomes [27, 16].
+
+// Start with an array of numbers and create a new array with each number divided by 2.
+// For example, [1, 2, 3] becomes [0.5, 1.0, 1.5].
+
+// Start with an array of strings and create a new array with each string's first letter only.
+// For example, ["hello", "goodbye"] becomes ["h", "g"].
+
+// Start with an array of hashes and create a new array of number values from each hash's :age key times 2.
+// For example, [{name: "Alice", age: 27}, {name: "Blane", age: 16}] becomes [54, 32].
+
+// Start with an array of numbers and create a new array with each number converted into a string.
+// For example, [1, 2, 3] becomes ["1", "2", "3"].
--- dp.rb
+++ dp.rb
@@ -1,18 +1,46 @@
-city1 = "Rochester"
-city2 = "Fargo"
-city3 = "Worcester"
+# Start with an array of numbers and create a new array with each number times 3.
+# For example, [1, 2, 3] becomes [3, 6, 9].
 
-p "You should visit #{city1}, #{city2}, and #{city3}."
+nums = [1,2,3]
+p nums.map { |num| num * 3 }
 
-p "Enter a password"
-pw = gets.chomp
-p pw == "Joshua" ? "Shall we play a game?" : "Access denied."
-    if pw == "Joshua"
-      p "Shall we play a game?"
-    else
-      p "Access denied."
-    end
+# Start with an array of strings and create a new array with each string upcased.
+# For example, ["hello", "goodbye"] becomes ["HELLO", "GOODBYE"].
 
-title = "Catcher in the Rye"
-author = "JD Salinger"
-puts "'#{title}' was written by #{author}."
+strs = ["hello", "goodbye"]
+upcased = strs.map { |str| str.upcase }
+p upcased
+
+# Start with an array of hashes and create a new array of string values from each hash's :name key.
+# For example, [{name: "Alice", age: 27}, {name: "Blane", age: 16}] becomes ["Alice", "Blane"].
+
+peeps = [{name: "Alice", age: 27}, {name: "Blane", age: 16}]
+names = peeps.map { |peep| peep[:name] }
+p names
+
+# Start with an array of numbers and create a new array with each number plus 7.
+# For example, [1, 2, 3] becomes [8, 9, 10].
+
+arr = [1,2,3]
+p arr.map { |num| num + 7 }
+
+# Start with an array of strings and create a new array with each string's length.
+# For example, ["hello", "goodbye"] becomes [5, 7].
+
+strs = ["hello", "goodbye"]
+p strs.map{ |str| str.length}
+
+# Start with an array of hashes and create a new array of number values from each hash's :age key.
+# For example, [{name: "Alice", age: 27}, {name: "Blane", age: 16}] becomes [27, 16].
+
+# Start with an array of numbers and create a new array with each number divided by 2.
+# For example, [1, 2, 3] becomes [0.5, 1.0, 1.5].
+
+# Start with an array of strings and create a new array with each string's first letter only.
+# For example, ["hello", "goodbye"] becomes ["h", "g"].
+
+# Start with an array of hashes and create a new array of number values from each hash's :age key times 2.
+# For example, [{name: "Alice", age: 27}, {name: "Blane", age: 16}] becomes [54, 32].
+
+# Start with an array of numbers and create a new array with each number converted into a string.
+# For example, [1, 2, 3] becomes ["1", "2", "3"].
