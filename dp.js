119c9b69c871fd09b53775224f9123707f1f57ec
John-Plough <jplough@gmail.com>
2025-03-17 21:15:59 -0400

    finish 1st half 03-loops3.md

--- dp.js
+++ dp.js
@@ -3,7 +3,7 @@
 
 const arr = [2, 32, 80, 18, 12, 3];
 const filtered = arr.filter((num) => num < 20);
-console.log(filtered);
+// console.log(filtered);
 
 // Start with an array of strings and create a new array with only the strings that start with the letter "w".
 // For example, ["winner", "winner", "chicken", "dinner"] becomes ["winner", "winner"].
@@ -16,7 +16,7 @@ words.forEach((word) => {
     ws.push(word);
   }
 });
-console.log(ws);
+// console.log(ws);
 
 // Start with an array of hashes and create a new array with only the hashes with prices greater than 5 (from the :price key).
 // For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes [{name: "chair", price: 100}].
@@ -33,15 +33,31 @@ items.forEach((item) => {
     exp.push(item);
   }
 });
-console.log(exp);
+// console.log(exp);
 
-console.log(items.filter((item) => item.price > 5));
+// console.log(items.filter((item) => item.price > 5));
 
 // Start with an array of numbers and create a new array with only the even numbers.
 // For example, [2, 4, 5, 1, 8, 9, 7] becomes [2, 4, 8].
 
+const arr1 = [2, 4, 5, 1, 8, 9, 7];
+const evens = [];
+for (let i = 0; i < arr1.length; i++) {
+  if (arr1[i] % 2 === 0) {
+    evens.push(arr1[i]);
+  }
+}
+console.log(evens);
+
+const evens1 = arr1.filter((num) => num % 2 === 0);
+console.log(evens1);
+
 // Start with an array of strings and create a new array with only the strings shorter than 4 letters.
 // For example, ["a", "man", "a", "plan", "a", "canal", "panama"] becomes ["a", "man", "a", "a"].
+
+const shorts = ["a", "man", "a", "plan", "a", "canal", "panama"];
+const supshorts = shorts.filter((word) => word.length < 4);
+console.log(supshorts);
 
 // Start with an array of hashes and create a new array with only the hashes with names shorter than 6 letters (from the :name key).
 // For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes [{name: "chair", price: 100}, {name: "book", price: 4}].
--- dp.rb
+++ dp.rb
@@ -2,7 +2,7 @@
 # For example, [2, 32, 80, 18, 12, 3] becomes [2, 18, 12, 3].
 
 arr = [2, 32, 80, 18, 12, 3]
-p arr.select { |num| num < 20 }
+# p arr.select { |num| num < 20 }
 
 # Start with an array of strings and create a new array with only the strings that start with the letter "w".
 # For example, ["winner", "winner", "chicken", "dinner"] becomes ["winner", "winner"].
@@ -14,10 +14,10 @@ arr.each do |word|
     ws << word
   end
 end
-p ws
+# p ws
 
-p arr.select { |word| word.downcase.start_with? "w" }
-p arr.select { |word| word[0].downcase == "w" }
+# p arr.select { |word| word.downcase.start_with? "w" }
+# p arr.select { |word| word[0].downcase == "w" }
 
 # Start with an array of strings and create a new array with only the strings that start with the letter "w".
 # For example, ["winner", "winner", "chicken", "dinner"] becomes ["winner", "winner"].
@@ -32,7 +32,7 @@ while i < words.length
   end
   i += 1
 end
-p ws
+# p ws
 
 words = ["winner", "winner", "Winner", "chicken", "dinner"]
 ws = []
@@ -42,19 +42,19 @@ words.each do |word|
     ws << word
   end
 end
-p ws
+# p ws
 
 words = ["winner", "winner", "Winner", "chicken", "dinner"]
 ws = words.select { |word| word[0].downcase == 'w' }
-p ws
+# p ws
 wss = words.select do |word|
   word[0].downcase == 'w'
 end
-p wss
+# p wss
 
 words = ["winner", "winner", "Winner", "Winner", "chicken", "dinner"]
 ws = words.select { |word| word.downcase.start_with? 'w' }
-p ws
+# p ws
 
 # Start with an array of hashes and create a new array with only the hashes with prices greater than 5 (from the :price key).
 # For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes [{name: "chair", price: 100}].
@@ -67,21 +67,31 @@ items.each do |item|
     exp << item
   end
 end
-p exp
+# p exp
 
-p items.select { |item| item[:price] > 5 }
+# p items.select { |item| item[:price] > 5 }
 
 
 # Start with an array of numbers and create a new array with only the even numbers.
 # For example, [2, 4, 5, 1, 8, 9, 7] becomes [2, 4, 8].
 
+arr = [2, 4, 5, 1, 8, 9, 7]
 
+evens = []
+arr.each do |num|
+  if num % 2 == 0
+    evens << num
+  end
+end
+# p evens
 
+# p arr.select { |num| num.even? }
 
 # Start with an array of strings and create a new array with only the strings shorter than 4 letters.
 # For example, ["a", "man", "a", "plan", "a", "canal", "panama"] becomes ["a", "man", "a", "a"].
 
-
+arr = ["a", "man", "a", "plan", "a", "canal", "panama"]
+p arr.select  { |str| str.length < 4 } 
 
 
 # Start with an array of hashes and create a new array with only the hashes with names shorter than 6 letters (from the :name key).
@@ -110,71 +120,3 @@ p items.select { |item| item[:price] > 5 }
 
 # Start with an array of numbers and create a new array with only the odd numbers.
 # For example, [2, 4, 5, 1, 8, 9, 7] becomes [5, 1, 9, 7].
-
-
-
-# Codewars - Maximum Length Difference
-
-def mxdiflg(a1, a2)
-
-  return -1 if a1.empty? || a2.empty?
-
-  a1max = -Float::INFINITY
-  a1min = Float::INFINITY
-  a2max = -Float::INFINITY
-  a2min = Float::INFINITY
-
-  a1.each do |str|
-    l = str.length
-    a1max = l if l > a1max
-    a1min = l if l < a1min
-  end
-
-  a2.each do |str|
-    l = str.length
-    a2max = l if l > a2max
-    a2min = l if l < a2min
-  end
-
-  spread1 = a1max - a2min
-  spread2 = a2max - a1min
-
-  return [spread1, spread2].max.to_s
-end
-
-
-
-def mxdiflg(a1, a2)
-  return -1 if a1.empty? || a2.empty?
-
-  min1, max1 = a1.map(&:size).minmax
-  min2, max2 = a2.map(&:size).minmax
-
-  [max1 - min2, max2 - min1].max
-end
-
-arr1 = ["hoqq", "bbllkw", "oox", "ejjuyyy", "plmiis", "xxxzgpsssa", "xxwwkktt", "znnnnfqknaz", "qqquuhii", "dvvvwz"]
-arr2 = ["cccooommaaqqoxii", "gggqaffhhh", "tttoowwwmmww"]
-p mxdiflg(arr1, arr2)
-
-# Codewars - USD => CNY
-
-def usdcny(usd)
-  # yuan = multiply by 6.75. to two decimal places
-  unrounded_yuan = usd * 6.75
-  yuan = sprintf("%.2f", unrounded_yuan)
-  # return yuan + sentence
-  "#{yuan} Chinese Yuan"
-end
-
-def usdcny(usd)
-  "#{"%.2f" %(usd * 6.75)} Chinese Yuan"
-end
-
-p usdcny(465)
-
-def usdcny(usd)
-  "#{"%.2f" %(usd * 6.75)} Chinese Yuan"
-end
-
-p usdcny(15)
