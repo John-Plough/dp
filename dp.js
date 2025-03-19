0dfce880cf3e64bf4523681f548173e8eaf80bd2
John-Plough <jplough@gmail.com>
2025-03-18 22:22:20 -0400

    03-loops4.md, first five

--- dp.js
+++ dp.js
@@ -6,12 +6,12 @@
 const arr = [5, 10, 8, 3];
 
 let total = arr.reduce((acc, num) => acc + num, 0);
-console.log(total);
+// console.log(total);
 
 const sum = arr.reduce(function (acc, num) {
   return acc + num;
 }, 0);
-console.log(sum);
+// console.log(sum);
 
 // Start with an array of strings and combine them all into a single string.
 // For example, ["volleyball", "basketball", "badminton"] becomes "volleyballbasketballbadminton".
@@ -19,9 +19,9 @@ console.log(sum);
 const sports = ["volleyball", "basketball", "badminton"];
 
 const string = sports.reduce((str, sport) => str + sport, "");
-console.log(string);
+// console.log(string);
 const string1 = sports.join("");
-console.log(string1);
+// console.log(string1);
 
 // Start with an array of hashes and compute the sum of the prices (from the :price key).
 // For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes 105.
@@ -33,19 +33,43 @@ const items = [
 ];
 
 total = items.reduce((total, item) => (total += item.price), 0);
-console.log(total);
+// console.log(total);
 
 let sum1 = 0;
 for (let i = 0; i < items.length; i++) {
   sum1 += items[i].price;
 }
-console.log(sum1);
+// console.log(sum1);
 
 // Start with an array of numbers and compute the the minimum number.
 // For example, [5, 10, 8, 3, 9] becomes 3.
 
+const myNums = [5, 10, 8, 3, 9];
+let myMin = Infinity;
+
+for (let i = 0; i < myNums.length; i++) {
+  if (myNums[i] < myMin) {
+    myMin = myNums[i];
+  }
+}
+// console.log(myMin);
+
+let min = myNums.reduce((low, num) => (num < low ? num : low), Infinity);
+// console.log(min);
+
 // Start with an array of strings and compute the total length of all the strings.
 // For example, ["volleyball", "basketball", "badminton"] becomes 29.
+
+const games = ["volleyball", "basketball", "badminton"];
+total = games.reduce((sum, game) => (sum += game.length), 0);
+
+let letters = 0;
+for (let i = 0; i < games.length; i++) {
+  letters += games[i].length;
+}
+// console.log(letters);
+
+// console.log(total);
 
 // Start with an array of hashes and find the hash with the lowest price (from the :price key).
 // For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes {name: "pencil", price: 1}.
--- dp.rb
+++ dp.rb
@@ -11,36 +11,64 @@ while i < arr.length
   total += arr[i]
   i += 1
 end
-p total
+# p total
 
-p arr.reduce(0) { |acc, num| acc + num }
+# p arr.reduce(0) { |acc, num| acc + num }
 
-p arr.sum
+# p arr.sum
 
 # Start with an array of strings and combine them all into a single string.
 # For example, ["volleyball", "basketball", "badminton"] becomes "volleyballbasketballbadminton".
 
 arr = ["volleyball", "basketball", "badminton"]
 
-p arr.reduce("") { |str, sport| str += sport }
+# p arr.reduce("") { |str, sport| str += sport }
 
 # Start with an array of hashes and compute the sum of the prices (from the :price key).
 # For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes 105.
 
 items = [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}]
-p items.reduce(0) { |total, item| total += item[:price] }
+# p items.reduce(0) { |total, item| total += item[:price] }
 
 sum = 0
 items.each { |item| sum += item[:price] }
-p sum
+# p sum
 
 # Start with an array of numbers and compute the the minimum number.
 # For example, [5, 10, 8, 3, 9] becomes 3.
 
+arr = [5, 10, 8, 3, 9]
+
+min = Float::INFINITY
+i = 0
+while i < arr.length
+  if arr[i] < min
+    min = arr[i]
+  end
+  i += 1
+end
+# p min
+
+min = arr.reduce(Float::INFINITY) do |low, num|
+  num < low ? num : low
+end
+# p min
+
+# p arr.min
 
 # Start with an array of strings and compute the total length of all the strings.
 # For example, ["volleyball", "basketball", "badminton"] becomes 29.
 
+arr = ["volleyball", "basketball", "badminton"]
+
+total = 0
+arr.each do |str|
+  total += str.length
+end
+p total
+
+total = arr.reduce(0) { |total, str| total += str.length }
+p total
 
 # Start with an array of hashes and find the hash with the lowest price (from the :price key).
 # For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes {name: "pencil", price: 1}.
