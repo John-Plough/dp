a6d6e500f276cdee8ed8c895a1db8f732fd6980a
John-Plough <jplough@gmail.com>
2025-03-29 23:10:24 -0400

    codewars

--- dp.rb
+++ dp.rb
@@ -1,134 +1,156 @@
-# def _if(bool, ifTrue, ifFalse)
-#   bool ? ifTrue : ifFalse
-# end
+# # # def angle(n)
 
-# def if(val, cb1, cb2)
-#   val ? cb1 : cb2
-# end
+# # # end
 
 
-  
-  # loop over str
-    # if consonant, add char.ord - 96 to sub
-    # else 
-      # if sub > high
-        # high = sub
-      # sub = 0
-  # return high
+# # # p "\n".match?(/[^[:alpha:]]/)
+# # # p "1".match?(/[^[:alpha:]]/)
+# # # p "B".match?(/[^[:alpha:]]/)
+
+# # # if "\n".match?(/[^[:alpha:]]/) || "b".match?(/[^[:alpha:]]/)
+# # #   p -1
+# # # end
+
+# # def same_case(a, b)
+# #   return -1 unless (a+b).match?(/\A[a-zA-Z]{2}\z/)
+
+# #   if (a.match?(/[a-z]/)) && (b.match?(/[a-z]/)) || (a.match?(/[A-Z]/)) && (b.match?(/[A-Z]/))
+# #     return 1
+# #   else
+# #     return 0
+# #   end
+# # end
+
+# # def same_case(a, b)
+# #   return -1 unless (a+b).match?(/\A[a-zA-Z]{2}\z/)
+
+# #   a.match?(/[a-z]/) && b.match?(/[a-z]/) ? 1 : 0
+# #   # if (a.match?(/[a-z]/)) && (b.match?(/[a-z]/)) || (a.match?(/[A-Z]/)) && (b.match?(/[A-Z]/))
+# #   #   return 1
+# #   # else
+# #   #   return 0
+# #   # end
+# # end
+
+# # def same_case(a, b)
+# #   return -1 unless (a+b).match?(/\A[a-zA-Z]{2}\z/)
+# #   (a == a.upcase) == (b == b.upcase) ? 1 : 0
+# # end
 
 
 
-# def solve(s)
-#   vowels = ['a', 'e', 'i', 'o', 'u']
-#   high = 0
-#   sub = 0
-
-#   i = 0
-#   while i < s.length
-#     char = s[i]
-#     if !vowels.include?(char) # if not a vowel
-#       sub += (char.ord - 96) # add char val to sub
-#     else
-#       if sub > high
-#         high = sub
-#       end
-#       sub = 0
-#     end
-#     i += 1
+# def bouncing_ball(h, bounce, window)
+#   if h <= 0 || bounce <= 0 || bounce >= 1 || window >= h
+#     return -1
 #   end
-#   high
-# end
 
+#   views = 1
 
-# def solve(s)
-#   vowels = ['a', 'e', 'i', 'o', 'u']
-#   high = 0
-#   sub = 0
-
-#   s.each_char do |char|
-#     if !vowels.include?(char) # if NOT a vowel
-#       sub += (char.ord - 96) # add char val to sub
-#     else # if vowel
-#       if sub > high
-#         high = sub
-#       end
-#       sub = 0 # reset sub to 0 for next substring
-#     end
+#   while h *= bounce > window
+#     views += 2
 #   end
-#   if sub > high
-#     high = sub
-#   end
-#   high
+
+#   views
 # end
 
 
-# p solve('zodiac')
+# # (3, .66, 1.5) --> 3
 
 
-# def solve(s)
-#   s.split(/[aeiou]/).map{|x| x.sum - x.size * 96}.max
-# end
+def moveZeros(arr) 
+  # loop over
+    # if 0, add to zero_total
+    # else push to new arr
+  # while loop to add 0s to new arr
+
+  result = arr.select { |num| num != 0 }
+  zeros = arr.select { |num| num == 0 }
+  result.concat(zeros)
+end
 
 
 
-# def solve(s)
-#   s.split(/[aeiou]/).map{ |substring| substring.sum - (substring.size * 96) }.max
-# end
+# p moveZeros [1,2,0,1,0,1,0,3,0,1] # --> [1,2,1,1,3,1,0,0,0,0]
+# puts "00".to_i(16)
 
-# def solve(str)
-#   # create array of consonant substrings
-#   subs_arr = str.split(/[aeiou]/) 
+# puts "FF".to_i(16)
 
-#   # create array of substring values
-#   vals = subs_arr.map { |substring| substring.sum - (substring.size * 96) }
+# puts 255.to_s(16).upcase
 
-#   # return highest value
-#   vals.max
-# end
+# puts r.to_s(16).upcase
 
-s = "Fred:Corwill;Wilfred:Corwill;Barney:Tornbull;Betty:Tornbull;Bjon:Tornbull;Raphael:Corwill;Alfred:Corwill";
-
-def meeting(s)
-  names = s.downcase.split(';').map { |name| name.split(':') }
-
-  sorted = names.sort do |a, b|
-    last = a[1] <=> b[1]
-    last == 0 ? a[0] <=> b[0] : last
+def rgb(r, g, b)
+  if r < 0
+    r = 0
+  elsif r > 255
+    r = 255
   end
 
-  sorted.map { |name| "(#{name[1].upcase}, #{name[0].upcase})" }.join
+  if g < 0
+    g = 0
+  elsif g > 255
+    g = 255
+  end
+
+  if b < 0
+    b = 0
+  elsif b > 255
+    b = 255
+  end
+
+  result = ''
+  rhex = r.to_s(16).upcase
+  ghex = g.to_s(16).upcase
+  bhex = b.to_s(16).upcase
+
+  if rhex.size < 2
+    result += "0" + rhex
+  else
+    result += rhex
+  end
+
+  if ghex.size < 2
+    result += "0" + ghex
+  else
+    result += ghex
+  end
+
+  if bhex.size < 2
+    result += "0" + bhex
+  else
+    result += bhex
+  end
+
+  result
 end
 
-# more readable version, with intermediate variables
-# def meeting(s)
-#   names = s.downcase.split(';').map { |name| name.split(':') }
+p rgb(0,9,14)
 
-#   sorted = names.sort_by { |name| [name[1], name[0]] }
+# p 259.to_s(16).upcase
 
-#   formatted_names = sorted.map { |name| "(#{name[1].upcase}, #{name[0].upcase})" }
+# p "FF".to_i < 5
 
-#   formatted_names.join
-# end
+# p "10".size
 
 
-def meeting(s)
-  s.upcase.split(';')
-    .map { |name| name.split(':') } # names
-    .sort_by { |name| [name[1], name[0]] } # sorted
-    .map { |name| "(#{name[1]}, #{name[0]})" } # formatted
-    .join # joined & returned
+def rgb(r, g, b)
+  [r, g, b]
+    .map { |val| [[val, 0].max, 255].min } 
+    .map { |val| val.to_s(16).rjust(2, '0').upcase } 
+    .join
 end
 
-# more readable version, with intermediate variables
-# def meeting(s)
-#   names = s.downcase.split(';').map { |name| name.split(':') }
 
-#   sorted = names.sort_by { |name| [name[1], name[0]] }
-
-#   formatted_names = sorted.map { |name| "(#{name[1].upcase}, #{name[0].upcase})" }
-
-#   formatted_names.join
-# end
+[r, g, b].map { |val| [[val, 0].max, 255].min } # clamp vals
 
 
-p meeting(s)
+Please use the adjustments from the previous questions. And please just separate numbers with a space instead of a comma and a space.
+
+numbers = {34 59 60 30 63 94 93 77 75 99}
+partition(numbers, 0, 4) is called.
+Assume quicksort always chooses the element at the midpoint as the pivot.
+
+What is the pivot?
+What is the low partition?
+What is the high partition?
+What is numbers after partition(numbers, 0, 4) completes?
