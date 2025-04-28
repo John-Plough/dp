460d8d5cfddaa1c732ad6d78f15509311e262ac6
John-Plough <jplough@gmail.com>
2025-04-28 12:30:10 -0400

    hackerrank

--- dp.js
+++ dp.js
@@ -1,37 +1,18 @@
-function plusMinus(arr) {
-  let pos = 0;
-  let neg = 0;
-  let zero = 0;
+// find sum (use reduce)
+// find min
+// find max
+// maxSum = sum - min
+// minSum = sum - max
 
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] > 0) {
-      pos++;
-    } else if (arr[i] < 0) {
-      neg++;
-    } else {
-      zero++;
-    }
-  }
-  console.log((pos / arr.length).toFixed(6));
-  console.log((neg / arr.length).toFixed(6));
-  console.log((zero / arr.length).toFixed(6));
+function miniMaxSum(arr) {
+  let sum = arr.reduce((acc, curVal) => acc + curVal, 0);
+  let min = Math.min(...arr);
+  let max = Math.max(...arr);
+
+  let maxSum = sum - min;
+  let minSum = sum - max;
+
+  console.log(`${minSum} ${maxSum}`);
 }
 
-function plusMinus(arr) {
-  let pos = 0;
-  let neg = 0;
-  let zero = 0;
-  let len = arr.length;
-
-  for (const num of arr) {
-    if (num > 0) pos++;
-    else if (num < 0) neg++;
-    else zero++;
-  }
-
-  console.log((pos / len).toFixed(6));
-  console.log((neg / len).toFixed(6));
-  console.log((zero / len).toFixed(6));
-}
-
-plusMinus([-4, 3, -9, 0, 4, 1]);
+miniMaxSum([1, 3, 5, 7, 9]);
